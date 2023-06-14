46dd33f09bc9038ec62fe40a218401f8d7f0202c
Galina <galina_87@mail.ru>
2023-06-15 00:42:29 +0300

    LR7

---
+++ LR7/CollectionExtension.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LR7
+{
+    public static class CollectionExtension
+    {
+        public static IEnumerable<T> TakeOdd<T>(this IEnumerable<T> collection)
+        {
+            int index = 0;
+            foreach (T element in collection)
+            {
+                if (index % 2 != 0)
+                {
+                    yield return element;
+                }
+
+                index++;
+            }
+        }
+
+        public static IEnumerable<T> TakeEven<T>(this IEnumerable<T> collection)
+        {
+            int index = 0;
+            foreach (T element in collection)
+            {
+                if (index % 2 == 0)
+                {
+                    yield return element;
+                }
+
+                index++;
+            }
+        }
+
+        public static IEnumerable<T> TakeNegative<T>(this IEnumerable<T> collection)
+        {
+            foreach (T element in collection)
+            {
+                Type objType = element.GetType();
+                objType = Nullable.GetUnderlyingType(objType) ?? objType;
+
+                if (objType.IsPrimitive
+                    && objType != typeof(bool) 
+                    && objType != typeof(char)
+                    && objType != typeof(IntPtr) 
+                    && objType != typeof(UIntPtr)
+                    || objType == typeof(decimal))
+                {
+                    if (Decimal.Parse(element.ToString()) < 0)
+                    {
+                        yield return element;
+                    }
+                }
+            }
+        }
+
+        public static IEnumerable<T> TakePositive<T>(this IEnumerable<T> collection)
+        {
+            foreach (T element in collection)
+            {
+                Type objType = element.GetType();
+                objType = Nullable.GetUnderlyingType(objType) ?? objType;
+
+                if (objType.IsPrimitive
+                    && objType != typeof(bool)
+                    && objType != typeof(char)
+                    && objType != typeof(IntPtr)
+                    && objType != typeof(UIntPtr)
+                    || objType == typeof(decimal))
+                {
+                    if (Decimal.Parse(element.ToString()) >= 0)
+                    {
+                        yield return element;
+                    }
+                }
+            }
+        }
+
+        public static IEnumerable<double> SmoothByMovingAverage(this IEnumerable<double> collection, int width)
+        {
+            var queue = new Queue<double>(width);
+            foreach (double element in collection)
+            {
+                if (queue.Count == width)
+                {
+                    queue.Dequeue();
+                }
+                queue.Enqueue(element);
+                
+                yield return queue.Average();
+              
+            }
+        }
+
+        public static IEnumerable<int> SmoothByMovingAverage(this IEnumerable<int> collection, int width)
+        {
+            var queue = new Queue<int>(width);
+            foreach (int element in collection)
+            {
+                if (queue.Count == width)
+                {
+                    queue.Dequeue();
+                }
+                queue.Enqueue(element);
+
+                yield return (int)queue.Average();
+
+            }
+        }
+
+        public static IEnumerable<decimal> SmoothByMovingAverage(this IEnumerable<decimal> collection, int width)
+        {
+            var queue = new Queue<decimal>(width);
+            foreach (int element in collection)
+            {
+                if (queue.Count == width)
+                {
+                    queue.Dequeue();
+                }
+                queue.Enqueue(element);
+
+                yield return (decimal)queue.Average();
+
+            }
+
+
+        }
+
+        public static void PrintEnum<T>(this IEnumerable<T> collection)
+        {
+            Console.WriteLine("Elements: ");
+            foreach (T element in collection)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("***");
+        }
+    }
+}
---
+++ LR7/Program.cs
@@ -0,0 +1,91 @@
+﻿
+using LR7;
+
+double[] doubles = new double[10];
+for (int i = 0; i < doubles.Length; i++)
+{
+    doubles[i] = (double)i*i-5;
+}
+
+Console.WriteLine("Начальный массив double:");
+doubles.PrintEnum();
+
+double[] d0 = doubles.TakeOdd().ToArray();
+Console.WriteLine("Массив TakeOdd():");
+d0.PrintEnum();
+
+double[] d1 = doubles.TakeEven().ToArray();
+Console.WriteLine("Массив TakeEven():");
+d1.PrintEnum();
+
+double[] d2 = doubles.TakeNegative().ToArray();
+Console.WriteLine("Массив TakeNegative():");
+d2.PrintEnum();
+
+double[] d3 = doubles.TakePositive().ToArray();
+Console.WriteLine("Массив TakePositive():");
+d3.PrintEnum();
+
+double[] d4 = doubles.SmoothByMovingAverage(4).ToArray();
+Console.WriteLine("Массив SmoothByMovingAverage(4):");
+d4.PrintEnum();
+
+
+decimal[] decimales = new decimal[10];
+for (int i = 0; i < decimales.Length; i++)
+{
+    decimales[i] = (decimal)i * i / 5;
+}
+
+Console.WriteLine("Начальный массив decimal:");
+decimales.PrintEnum();
+
+decimal[] dec0 = decimales.TakeOdd().ToArray();
+Console.WriteLine("Массив TakeOdd():");
+dec0.PrintEnum();
+
+decimal[] dec1 = decimales.TakeEven().ToArray();
+Console.WriteLine("Массив TakeEven():");
+dec1.PrintEnum();
+
+decimal[] dec2 = decimales.TakeNegative().ToArray();
+Console.WriteLine("Массив TakeNegative():");
+dec2.PrintEnum();
+
+decimal[] dec3 = decimales.TakePositive().ToArray();
+Console.WriteLine("Массив TakePositive():");
+dec3.PrintEnum();
+
+decimal[] dec4 = decimales.SmoothByMovingAverage(4).ToArray();
+Console.WriteLine("Массив SmoothByMovingAverage(4):");
+dec4.PrintEnum();
+
+
+int[] ints = new int[10];
+for (int i = 0; i < ints.Length; i++)
+{
+    ints[i] = i * i - 7;
+}
+
+Console.WriteLine("Начальный массив int:");
+ints.PrintEnum();
+
+int[] i0 = ints.TakeOdd().ToArray();
+Console.WriteLine("Массив TakeOdd():");
+i0.PrintEnum();
+
+int[] i1 = ints.TakeEven().ToArray();
+Console.WriteLine("Массив TakeEven():");
+i1.PrintEnum();
+
+int[] i2 = ints.TakeNegative().ToArray();
+Console.WriteLine("Массив TakeNegative():");
+i2.PrintEnum();
+
+int[] i3 = ints.TakePositive().ToArray();
+Console.WriteLine("Массив TakePositive():");
+i3.PrintEnum();
+
+int[] i4 = ints.SmoothByMovingAverage(4).ToArray();
+Console.WriteLine("Массив SmoothByMovingAverage(4):");
+i4.PrintEnum();

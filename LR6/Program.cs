5af278542767ed52b684d166826cba1caff6e611
Galina <galina_87@mail.ru>
2023-06-14 22:44:55 +0300

    LR6 without an additional task

---
+++ LR6/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using LR6;
+
+SinglyLinkedList<int> ints = new SinglyLinkedList<int>();
+
+for (int j  = 0; j < 10; j++)
+{
+    ints.Add(j);
+}
+foreach (int i in ints)
+{
+    Console.WriteLine(i);
+}
+
+ints.Remove(8);
+foreach (int i in ints)
+{
+    Console.WriteLine(i);
+}
---
+++ LR6/SinglyLinkedList.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LR6
+{
+    public class SinglyLinkedList<T> : IEnumerable<T>
+    {
+        Node<T>? head; 
+        Node<T>? tail; 
+        int count;  
+
+        
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+
+            if (head == null)
+                head = node;
+            else
+                tail!.Next = node;
+            tail = node;
+
+            count++;
+        }
+       
+        public bool Remove(T data)
+        {
+            Node<T>? current = head;
+            Node<T>? previous = null;
+
+            while (current != null && current.Data != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    
+                    if (previous != null)
+                    {
+                        previous.Next = current.Next;
+
+                        if (current.Next == null)
+                            tail = previous;
+                    }
+                    else
+                    {
+                        head = head?.Next;
+
+                        if (head == null)
+                            tail = null;
+                    }
+                    count--;
+                    return true;
+                }
+
+                previous = current;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public int Count { get { return count; } }
+        public bool IsEmpty { get { return count == 0; } }
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+        public bool Contains(T data)
+        {
+            Node<T>? current = head;
+            while (current != null && current.Data != null)
+            {
+                if (current.Data.Equals(data)) return true;
+                current = current.Next;
+            }
+            return false;
+        }
+        public void AppendFirst(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            node.Next = head;
+            head = node;
+            if (count == 0)
+                tail = head;
+            count++;
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node<T>? current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)this).GetEnumerator();
+        }
+    }
+}
